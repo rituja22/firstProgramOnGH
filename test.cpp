df5320165e34f91f431a2cd88eba0a2cd221e9d9
rituja22 <rituja.works@gmail.com>
2022-07-23 02:44:33 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> ab(4,-1);
+    for (int i=0;i<ab.size();i++)
+    {
+        cout<<ab[i]<<endl;
+    }
+    return 0;
+}
